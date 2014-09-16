050876f39d0091a0f9e0789ca5f110d106f4e724
Ilya Zayats <somebody32@gmail.com>
2014-09-16 11:41:06 +0500

    scratching traversing down

--- lib/twitter-fetchr.rb
+++ lib/twitter-fetchr.rb
@@ -10,4 +10,23 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
 end
 
-puts client.mentions_timeline(trim_user: true).map &:text
+# there is no saved tweet, so we go deep. Need a key to check that
+# also an option to wait for a new tweets without traversing
+begin
+  # get the first tweet
+  tweets = client.mentions_timeline(count: 1)
+  puts tweets.map &:text
+  max_id = tweets.last.id
+
+  # paginate
+  while tweets = client.mentions_timeline(max_id: max_id - 1, count: 5) do
+    break if tweets.empty?
+    puts tweets.map &:text
+    max_id = tweets.last.id
+  end
+rescue Twitter::Error::TooManyRequests => error
+  # what if error encounters while traversing down?
+  puts error.rate_limit.reset_in + 1
+end
+
+
