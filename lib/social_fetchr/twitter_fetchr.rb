3c2b35eb72ff3eafee9aba4b0707ec43a4bf4a57
Ilya Zayats <somebody32@gmail.com>
2014-09-17 13:44:30 +0500

    method extractions for twitter fetchr

--- lib/social_fetchr/twitter_fetchr.rb
+++ lib/social_fetchr/twitter_fetchr.rb
@@ -16,32 +16,45 @@ module SocialFetchr
     DEFAULT_ERROR_HANDLER = ->(error) { sleep error.rate_limit.reset_in + 1 }
 
     def fetch_all(count: 200, error_handler: DEFAULT_ERROR_HANDLER)
-      begin
-        all_tweets = client.mentions_timeline(count: count)
-      rescue Twitter::Error::TooManyRequests => e
-        error_handler.call(e)
-        retry
-      end
-
+      all_tweets = get_initial_batch(count, error_handler)
       return all_tweets if all_tweets.size < count
-      max_id = all_tweets.last.id
+      paginate_deep_starting(all_tweets, count, error_handler)
+    end
 
+    private
+
+    def get_initial_batch(count, error_handler)
+      client.mentions_timeline(count: count)
+    rescue Twitter::Error::TooManyRequests => e
+      error_handler.call(e)
+      retry
+    end
+
+    # it is possible to split this method even more, but for my taste
+    # this will introduce more eye-hops when grasping on it.
+    # And it is a very easy one, so it is better to disable rubocop's warnings
+    # rubocop:disable Metrics/MethodLength
+    def paginate_deep_starting(tweets, count, error_handler)
+      max_id = tweets.last.id
       begin
         loop do
           # need to substract 1 from max_id to avoid duplicating the last
           # tweet from the previous batch. This is an 'official' hack:
           # https://dev.twitter.com/rest/public/timelines
-          next_batch = client.mentions_timeline(max_id: max_id - 1, count: count)
-          all_tweets.concat(next_batch)
+          next_batch = client.mentions_timeline(
+            max_id: max_id - 1,
+            count: count
+          )
+          tweets.concat(next_batch)
           break if next_batch.empty? || next_batch.size < count
           max_id = next_batch.last.id
         end
+        tweets
       rescue Twitter::Error::TooManyRequests => e
         error_handler.call(e)
         retry
       end
-
-      all_tweets
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
