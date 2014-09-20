43a99ad8d575395485b890e16e3db3575c1cbcbd
Ilya Zayats <somebody32@gmail.com>
2014-09-20 13:02:52 +0500

    refactored fetchr core to class

--- lib/social_fetchr/fetchr.rb
+++ lib/social_fetchr/fetchr.rb
@@ -4,46 +4,56 @@ require "social_fetchr/post_scrubbr"
 
 module SocialFetchr
   class Fetchr
+    attr_reader :client_key, :social_adapter
+
     def self.check_updates(social_credentials)
-      client_key = social_credentials.fetch(:client_key)
-      tw_adapter = initialize_social_adapter(social_credentials)
-
-      post_to_store = check_and_process_posts(client_key, tw_adapter)
-
-      return unless post_to_store
-      store_last_post(client_key, post_to_store)
+      new(social_credentials).check_and_process_updates
     end
 
+    def initialize(social_credentials)
+      @client_key = social_credentials.fetch(:client_key)
+      @social_adapter = initialize_social_adapter(social_credentials)
+    end
 
-    def self.initialize_social_adapter(credentials)
+    def check_and_process_updates
+      last_processed_post = process_new_posts
+      store_last_post(last_processed_post)
+    end
+
+    private
+
+    def initialize_social_adapter(credentials)
       SocialFetchr::TwitterFetchr.new(credentials)
     end
 
-    def self.check_and_process_posts(client_key, adapter)
+    def process_new_posts
       last_post =
         SocialFetchr::PostTrackr.last_processed_post(client: client_key)
 
-      post_to_store = if last_post
-                        process_new_posts_since(last_post, adapter)
-                      else
-                        get_latest_post(adapter)
-                      end
-      post_to_store
+      if last_post
+        process_new_posts_since(last_post)
+      else
+        latest_post
+      end
     end
 
-    def self.get_latest_post(adapter)
-      adapter.fetch_all(count: 1).first
+    def latest_post
+      social_adapter.fetch_all(count: 1).first
     end
 
-    def self.process_new_posts_since(last_post, adapter)
-      new_posts = adapter.fetch_since(since_id: last_post)
-      new_posts.reverse.each do |post|
+    def process_new_posts_since(last_post)
+      process_posts(social_adapter.fetch_since(since_id: last_post))
+    end
+
+    def process_posts(posts)
+      posts.reverse.each do |post|
         TaskPostr.post_task SocialFetchr::PostScrubbr.scrub(post.text)
       end
-      new_posts.first
+      posts.first
     end
 
-    def self.store_last_post(client_key, post)
+    def store_last_post(post)
+      return unless post
       SocialFetchr::PostTrackr.store_last_processed_post(
         client: client_key,
         post_id: post.id
