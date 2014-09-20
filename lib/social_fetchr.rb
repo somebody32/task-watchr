0dc1f9f494b3e5a90d5b4fc87eca9178f6446946
Ilya Zayats <somebody32@gmail.com>
2014-09-20 12:46:40 +0500

    massaged structure for a social fetchr
    Don't like the result, should refactor everything to a normal
    class with one public method that will kick the constructor.
    
    That will remove all these vars and private methods pain

--- lib/social_fetchr.rb
+++ lib/social_fetchr.rb
@@ -5,31 +5,52 @@ require "social_fetchr/post_scrubbr"
 module SocialFetchr
   module_function
 
-  # TODO
-  # 3. clean up this mess
-
   def check_updates(social_credentials)
     client_key = social_credentials.fetch(:client_key)
-    tw_client = SocialFetchr::TwitterFetchr.new(social_credentials)
+    tw_adapter = initialize_social_adapter(social_credentials)
 
+    post_to_store = check_and_process_posts(client_key, tw_adapter)
+
+    return unless post_to_store
+    store_last_post(client_key, post_to_store)
+  end
+
+
+  def initialize_social_adapter(credentials)
+    SocialFetchr::TwitterFetchr.new(credentials)
+  end
+
+  def check_and_process_posts(client_key, adapter)
     last_post =
       SocialFetchr::PostTrackr.last_processed_post(client: client_key)
 
-    if last_post
-      new_posts = tw_client.fetch_since(since_id: last_post)
-      post_to_store = new_posts.first
-      new_posts.reverse.each do |post|
-        TaskPostr.post_task SocialFetchr::PostScrubbr.scrub(post.text)
-      end
-    else
-      post_to_store = tw_client.fetch_all(count: 1).first
-    end
-
-    if post_to_store
-      SocialFetchr::PostTrackr.store_last_processed_post(
-        client: client_key,
-        post_id: post_to_store.id
-      )
-    end
+    post_to_store = if last_post
+                      process_new_posts_since(last_post, adapter)
+                    else
+                      get_latest_post(adapter)
+                    end
+    post_to_store
   end
+
+  def get_latest_post(adapter)
+    adapter.fetch_all(count: 1).first
+  end
+
+  def process_new_posts_since(last_post, adapter)
+    new_posts = adapter.fetch_since(since_id: last_post)
+    new_posts.reverse.each do |post|
+      TaskPostr.post_task SocialFetchr::PostScrubbr.scrub(post.text)
+    end
+    new_posts.first
+  end
+
+  def store_last_post(client_key, post)
+    SocialFetchr::PostTrackr.store_last_processed_post(
+      client: client_key,
+      post_id: post.id
+    )
+  end
+
+  private_class_method :initialize_social_adapter, :check_and_process_posts,
+    :get_latest_post, :process_new_posts_since, :store_last_post
 end
