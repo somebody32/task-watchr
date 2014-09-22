b4bb03585637fd664861c6adfab13dfa526409f1
Ilya Zayats <somebody32@gmail.com>
2014-09-22 12:44:56 +0500

    massaged token updater a little bit

--- lib/social_postr/token_updaters/redbooth.rb
+++ lib/social_postr/token_updaters/redbooth.rb
@@ -3,22 +3,36 @@ require "omniauth-redbooth"
 module SocialPostr
   module TokenUpdaters
     module Redbooth
+      URLS = {
+        site: "https://redbooth.com/api/3",
+        token_url: "https://redbooth.com/oauth2/token",
+        authorize_url: "https://redbooth.com/oauth2/authorize"
+      }
+
       module_function
 
-      def update_token(access_token, refresh_token)
-        client_id = ENV['REDBOOTH_KEY']
-        client_secret = ENV['REDBOOTH_SECRET']
-        oauth2_urls = {
-          site: 'https://redbooth.com/api/3',
-          token_url: 'https://redbooth.com/oauth2/token',
-          authorize_url: 'https://redbooth.com/oauth2/authorize'
-        }
-        oauth2_client = OAuth2::Client.new(client_id, client_secret, oauth2_urls)
-        access_token = OAuth2::AccessToken.new(oauth2_client, access_token)
-        refresh_access_token_obj = OAuth2::AccessToken.new(oauth2_client, access_token.token, {'refresh_token' => refresh_token})
-        access_token = refresh_access_token_obj.refresh!
-        {token: access_token.token, refresh_token: access_token.refresh_token }
+      def update_token(credentials)
+        client_id     = credentials.fetch(:key)
+        client_secret = credentials.fetch(:secret)
+        token         = credentials.fetch(:token)
+        refresh_token = credentials.fetch(:refresh_token)
+
+        oauth2_client = OAuth2::Client.new(client_id, client_secret, URLS)
+        new_token = request_new_token(oauth2_client, token, refresh_token)
+
+        { token: new_token.token, refresh_token: new_token.refresh_token }
       end
+
+      def request_new_token(client, current_token, refresh_token)
+        access_token = OAuth2::AccessToken.new(client, current_token)
+        token_refresher = OAuth2::AccessToken.new(
+          client,
+          access_token.token,
+          "refresh_token" => refresh_token
+        )
+        token_refresher.refresh!
+      end
+      private_class_method :request_new_token
     end
   end
 end
--- spec/lib/social_postr/token_updaters/redbooth_spec.rb
+++ spec/lib/social_postr/token_updaters/redbooth_spec.rb
@@ -12,9 +12,17 @@ VCR.configure do |c|
 end
 
 describe SocialPostr::TokenUpdaters::Redbooth do
+  let(:credentials) do
+    {
+      key:           ENV["REDBOOTH_KEY"],
+      secret:        ENV["REDBOOTH_SECRET"],
+      token:         ENV["REDBOOTH_TOKEN"],
+      refresh_token: ENV["REDBOOTH_REFRESH_TOKEN"]
+    }
+  end
   it "updates and returns a new token pair" do
     VCR.use_cassette("redbooth_token_update") do
-      new_tokens = described_class.update_token(ENV['REDBOOTH_TOKEN'], ENV['REDBOOTH_REFRESH_TOKEN'])
+      new_tokens = described_class.update_token(credentials)
       expect(new_tokens[:token]).not_to be_empty
       expect(new_tokens[:refresh_token]).not_to be_empty
     end
