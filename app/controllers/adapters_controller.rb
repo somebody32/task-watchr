81563ad62e2ae327ff51917ae3925830e6a42af2
Ilya Zayats <somebody32@gmail.com>
2014-09-22 18:10:27 +0500

    style fixes to make the build pass

--- app/controllers/adapters_controller.rb
+++ app/controllers/adapters_controller.rb
@@ -1,7 +1,5 @@
-require "pry"
 class AdaptersController < ApplicationController
   def connect
-    binding.pry
-    render text: request.env['omniauth.auth'].inspect
+    render text: request.env["omniauth.auth"].inspect
   end
 end
