52a6990d27e60440f6d26cb91bdf0b2a6ed282b0
Ilya Zayats <somebody32@gmail.com>
2014-09-24 20:10:13 +0500

    tuning redis connection pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 redis_conn = proc {
-  $redis
+  Redis.new(url: ENV["REDIS_URL"]) # do anything you want here
 }
 Sidekiq.configure_client do |config|
   config.redis = ConnectionPool.new(size: 1, &redis_conn)
 end
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 1, &redis_conn)
+  config.redis = ConnectionPool.new(size: 2, &redis_conn)
 end
